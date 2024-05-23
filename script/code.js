f30371749384884ecc0e1ed23a72489f22b3e329
lamlankosi <lamlanomnganga9@gmail.com>
2024-05-23 09:15:08 +0200

    done

---
+++ script/code.js
@@ -0,0 +1,25 @@
+function greet() {
+    console.log('hello');
+}
+
+greet()
+
+function multiply(numb1 , numb2) {
+    console.log(numb1 * numb2);
+}
+
+multiply(2,2)
+
+function tableOfFive(number) {
+    
+    let answer = number * 5;
+
+    console.log(answer);
+    
+    
+    return answer;
+}
+
+tableOfFive(2); 
+tableOfFive(4); 
+tableOfFive(7); 
